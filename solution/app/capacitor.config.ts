8718b2c94e39a3998f15294091715ef5a7f57fe3
Benjamin Fuentes <zamrokk@gmail.com>
2023-01-27 14:35:37 +0100

    adding capacitor icons

--- solution/app/capacitor.config.ts
+++ solution/app/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "dev.marigold.shifumi",
-  appName: "app",
+  appName: "Tezos Shifumi",
   webDir: "build",
   bundledWebRuntime: false,
 };
