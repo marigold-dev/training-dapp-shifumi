fc4203fa54b37158caec89c0ff8cf388d4b2e40c
Benjamin Fuentes <zamrokk@gmail.com>
2023-08-04 15:01:24 +0200

    remove backup folder

--- solutionOLD/app/android/app/src/main/java/dev/marigold/shifumi/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package dev.marigold.shifumi;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- solutionOLD/app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "dev.marigold.shifumi",
-  appName: "Tezos Shifumi",
-  webDir: "dist",
-};
-
-export default config;
